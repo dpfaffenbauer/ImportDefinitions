19e102ac3c7759d121997d3264c5869b1b133911
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2026-09-15 19:46:38 +0200

    move folder

---
+++ src/Resources/public/studio/1acf383d-d170-45f9-a288-8669a920a5a2/exposeRemote.js
@@ -0,0 +1,7 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      window.pluginRemotes.datadefinitions = "/bundles/datadefinitions/studio/1acf383d-d170-45f9-a288-8669a920a5a2/static/js/remoteEntry.js"
+    
